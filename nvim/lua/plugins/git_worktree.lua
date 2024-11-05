40d2a73673d34e81d3360102e563286e8197c563
ashish.khatke <khatkeashish@gmail.com>
2024-11-05 15:05:11 +0200

    add git worktree plugin

---
+++ nvim/lua/plugins/git_worktree.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    -- "ThePrimeagen/git-worktree.nvim",
+    "polarmutex/git-worktree.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    enabled = true,
+    opts = {
+      change_directory_command = "cd",
+      update_on_change = true,
+      update_on_change_command = "e .",
+      clearjumps_on_change = true,
+      autopush = false,
+    },
+    keys = {
+      -- add a keymap to browse plugin files
+      {
+        "<leader>zc",
+        function()
+          require("telescope").extensions.git_worktree.create_git_worktree()
+        end,
+        desc = "Create git worktree",
+      },
+      {
+        "<leader>zl",
+        function()
+          require("telescope").extensions.git_worktree.git_worktrees()
+        end,
+        desc = "Show, switch, delete git worktree",
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      -- "ThePrimeagen/git-worktree.nvim",
+      "polarmutex/git-worktree.nvim",
+    },
+  },
+}
