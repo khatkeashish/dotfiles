a6fbe03f6a0d6bbfd60f1e7c24ea9038a97ee53e
ashish.khatke <khatkeashish@gmail.com>
2024-11-05 15:04:56 +0200

    add rose-pine

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "rose-pine",
     },
   },
 }
